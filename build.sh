fab75d8d3d0036b51bd60cc53d384a3ee6a80d24
guoxbin <guoxbin@gmail.com>
2020-07-29 18:19:13 +0800

    rm java/oc

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 cargo lipo --release
 cbindgen src/ffi.rs -l c > target/universal/release/libyee_signer.h
 
-cp target/universal/release/libyee_signer.h oc/YeeSigner/YeeSigner/YeeSigner/
-cp target/universal/release/libyee_signer.a oc/YeeSigner/YeeSigner/YeeSigner/
+cp target/universal/release/libyee_signer.h ../oc/YeeSigner/YeeSigner/YeeSigner/
+cp target/universal/release/libyee_signer.a ../oc/YeeSigner/YeeSigner/YeeSigner/
 
 #java
 PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/:$PATH CC_aarch64_linux_android=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android26-clang CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android26-clang cargo build --lib --release --target aarch64-linux-android
@@ -13,7 +13,7 @@ PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/:$PATH CC_armv
 PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/:$PATH CC_i686_linux_android=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/i686-linux-android26-clang CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/i686-linux-android26-clang cargo build --lib --release --target i686-linux-android
 cargo build --release
 
-cp target/aarch64-linux-android/release/libyee_signer.so java/jniLibs/arm64/
-cp target/armv7-linux-androideabi/release/libyee_signer.so java/jniLibs/armeabi/
-cp target/i686-linux-android/release/libyee_signer.so java/jniLibs/x86/
-cp target/release/libyee_signer.dylib java/jniLibs/
+cp target/aarch64-linux-android/release/libyee_signer.so ../java/jniLibs/arm64/
+cp target/armv7-linux-androideabi/release/libyee_signer.so ../java/jniLibs/armeabi/
+cp target/i686-linux-android/release/libyee_signer.so ../java/jniLibs/x86/
+cp target/release/libyee_signer.dylib ../java/jniLibs/
--- java/src/main/java/io/yeeco/yeesigner/SignerException.java
+++
@@ -1,7 +0,0 @@
-package io.yeeco.yeesigner;
-
-public class SignerException extends Exception {
-    public SignerException(String message) {
-        super(message);
-    }
-}
